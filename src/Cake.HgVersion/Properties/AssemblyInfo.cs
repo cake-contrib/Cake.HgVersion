f0f4f846509096bba8970635f9066852d7642d7b
Kvachuk Grigoriy <violentcipher@gmail.com>
2017-11-06 21:28:20 +1000

    close #1

---
+++ src/Cake.HgVersion/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Cake.HgVersion")]
+[assembly: AssemblyDescription("Cake AddIn that extends Cake with SemVer versioning using Mercurial history")]
+[assembly: AssemblyCompany("vCipher")]
+[assembly: AssemblyProduct("Cake.HgVersion")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("FF5674B2-C3AC-4E82-8360-655D822ECCC5")]
---
+++ src/Cake.HgVersion/Properties/Namespaces.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+// ReSharper disable CheckNamespace
+// ReSharper disable UnusedMember.Global
+
+namespace Cake.HgVersion.Aliases
+{
+    /// <summary>
+    /// This namespace contains aliases for versioning projects
+    /// with semantic version standard <see href="http://semver.org"/>.
+    /// </summary>
+    [CompilerGenerated]
+    internal class Namespaces
+    { }
+}
