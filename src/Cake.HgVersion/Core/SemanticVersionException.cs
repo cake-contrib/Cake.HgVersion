f16f9c1fd0baab8acc6a75e61c09f1a814701002
Kvachuk Grigoriy <violentcipher@gmail.com>
2017-11-12 23:49:37 +1000

    Add SemanticVersion object model

---
+++ src/Cake.HgVersion/Core/SemanticVersionException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Cake.HgVersion.Core
+{
+    [Serializable]
+    public class SemanticVersionExceptionException : Exception
+    {
+        public SemanticVersionExceptionException() { }
+        public SemanticVersionExceptionException(string message) : base(message) { }
+        public SemanticVersionExceptionException(string message, Exception inner) : base(message, inner) { }
+
+        protected SemanticVersionExceptionException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        { }
+    }
+}
---
+++ src/Cake.HgVersion/Core/VersionField.cs
@@ -0,0 +1,11 @@
+﻿namespace Cake.HgVersion.Core
+{
+    
+    public enum VersionField
+    {
+        None,
+        Patch,
+        Minor,
+        Major
+    }
+}
